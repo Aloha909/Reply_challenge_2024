136e70d20233b1271b4e3db3663e5ea769c01527
Adam Guglielmino <121334742+Guguitino@users.noreply.github.com>
2024-03-21 17:21:46 +0100

    Mise à jour :)

--- Lecteur_v1.py
+++ Lecteur_v1.py
@@ -1,6 +1,8 @@
+from Point import GoldenPoint, SilverPoint
+
 class Lecteur_v1():
 
-    __slots__ = ["path", "data"]
+    __slots__ = ["path", "data", "width", "height", "nb_golden_point", "nb_silver_point", "nb_tiles_av", "golden_points", "silver_points", "tiles_av"]
 
     def __init__(self, path):
         self.path: str = path
@@ -14,3 +16,23 @@ class Lecteur_v1():
 
             for i in range(len(self.data)):
                 self.data[i] = self.data[i].split(" ")
+        
+    def process_data(self):
+        temp_data = self.data.copy()
+        self.width: int = int(self.data[0][0])
+        self.height: int = int(self.data[0][1])
+        self.nb_golden_point: int = int(self.data[0][2])
+        self.nb_silver_point: int = int(self.data[0][3])
+        self.nb_tiles_av: int = int(self.data[0][4])
+        temp_data = temp_data[1:]
+
+        for i in range(self.nb_golden_point):
+            x, y = temp_data[i]
+            self.golden_points = GoldenPoint(int(x), int(y))
+        temp_data = temp_data[self.nb_golden_point:]
+
+        for i in range(self.nb_silver_point):
+            x, y, value = temp_data[i]
+            self.silver_points = SilverPoint(int(x), int(y), int(value))
+        temp_data = temp_data[self.nb_silver_point:]
+        
