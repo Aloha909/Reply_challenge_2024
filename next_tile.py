f61e99dedef6967812068e0b225b3c121e7ceb7c
Aloha909 <alois.psw+git@gmail.com>
2024-03-21 20:05:33 +0100

    function complementaire

--- next_tile.py
+++ next_tile.py
@@ -1,5 +1,7 @@
 from Tiles import Tile
 
+def complementaire(dir):
+    return {'E': 'W', 'W': 'E', 'N': 'S', 'S': 'N'}[dir]
 
 def next_tile(entree: str, sortie: str, costs: dict, available_tiles: dict[str:int]) -> list:
     """
@@ -31,6 +33,8 @@ def next_dir(x_start, y_start, x_end, y_end):
             return "N"
         elif y_end < y_start :
             return "S"
+
+
 def goal_reached(x:int, y:int, x_goal:int, y_goal:int) -> bool:
     if abs(x_goal - x) == 1 and abs(y_goal - y) == 1:
         return True
